00868b5341f3f5a9c755b12aaffa3834e500cb7c
seanjmurray <sean.j.murray1@gmail.com>
2020-09-17 08:49:41 -0700

    add history and loading components and move state to body

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,20 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import md5 from 'md5'
 import Restform from './rest-form'
 import ResultsSection from './results'
+import HistorySection from './history'
 
 export default () => {
     const [results, setResults] = useState()
+    const [loading, setLoading] = useState(false)
+    const [history, setHistory] = useState({})
+    const [request, setRequest] = useState({})
 
-    const handleResults = (data) => {
-        setResults(data)
+    useEffect(() => {
+        setHistory(JSON.parse(localStorage.getItem('history')))
+    },[setHistory])
+
+    const handleLoad =  () => setLoading(loading => !loading)
+
+    const updateHistory = (req) => {
+        let hash = md5(JSON.stringify(req));
+
+        const newHistory = { ...history, [hash]: req }
+        setHistory(newHistory)
+        localStorage.setItem('history', JSON.stringify(newHistory));
+    }
+    
+    const updateRequest = (req) => {
+        setRequest(req)
+    }
+
+    const handleSubmit = (request) => {
+        const { method,url,body } = request
+        console.log('REQ', request)
+        setRequest(request)
+        handleLoad()
+        axios({
+            method: method,
+            url: url,
+            data: body ? body : null,
+          }).then(data => {
+            setResults(data)
+            handleLoad()
+            updateHistory(request)
+        }).catch(err => {
+            console.error(err)
+        })
     }
 
     return (
         <div className="main">
-            <Restform handleResults={handleResults} />
-            {results ?
-            <ResultsSection data={results} /> :
-            null}
+            <Restform req={request} handleSubmit={handleSubmit} />
+            <section className="flex">
+            <HistorySection history={history} handle={updateRequest} />
+            <ResultsSection loading={loading} data={results} />
+            </section>
         </div>
     )
 }
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactJson from 'react-json-view'
+import { WindMillLoading } from 'react-loadingg'
 import './css/results.scss'
 
 export default (props) => {
     return (
         <div className='results'>
+            {props.loading ?
+            <WindMillLoading /> :
+            props.data ?
+            <>
             <h3>Headers:</h3>
             <ReactJson src={props.data.headers} />
             <h3>Body:</h3>
             <ReactJson src={props.data.data} />
+            </> :
+            <WindMillLoading />}
+
         </div>
     )
 }
